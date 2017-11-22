95b921a6ff179d2a50dc44c531001a61374f98cd
Karl Bartel <karl42@gmail.com>
2017-11-22 11:19:49 +0100

    extract all text if subelements are present

--- tei2slob/__init__.py
+++ tei2slob/__init__.py
@@ -53,7 +53,7 @@ TAG_INCLUDE = '{http://www.w3.org/2001/XInclude}include'
 
 def text(parent, path):
     element = parent.find(path, NS_MAP)
-    return element.text if element is not None else ''
+    return ''.join(element.itertext())
 
 
 def attr(parent, path, attr_name):
